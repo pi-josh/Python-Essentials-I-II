5b3179bc8d1f27a1739d32c55aba040f5a38a1dd
pi-josh <joshua.macatunao007@gmail.com>
2024-03-09 01:00:29 +0800

    Finished 3.4.7 LAB Exercise

--- Python Essentials 2/Labs/3.4.7 - The Timer class.py
+++ Python Essentials 2/Labs/3.4.7 - The Timer class.py
@@ -41,7 +41,7 @@ Expected Output:
 '''
 
 class Timer:
-    def __init__(self, hours, minutes, seconds):
+    def __init__(self, hours = 0, minutes = 0, seconds = 0):
         self.__hours = hours
         self.__minutes = minutes
         self.__seconds = seconds
@@ -50,19 +50,55 @@ class Timer:
         return print_format(self)
 
     def next_second(self):
-        pass
-        #
-        # Write code here
-        #
+        self.__seconds += 1
+        if self.__seconds == 60:
+            self.__seconds = 0
+            self.__minutes += 1
+            if self.__minutes == 60:
+                self.__hours += 1
+                self.__minutes = 0
+                if self.__hours == 24:
+                    self.__hours = 0
+        
 
     def prev_second(self):
-        pass
-        #
-        # Write code here
-        #
+        self.__seconds -= 1
+        if self.__seconds == -1:
+            self.__seconds = 59
+            self.__minutes -= 1
+            if self.__minutes == -1:
+                self.__hours -= 1
+                self.__minutes = 59
+                if self.__hours == -1:
+                    self.__hours = 23
 
 def print_format(cls):
-    return f"{cls._Timer__hours}:{cls._Timer__minutes}:{cls._Timer__seconds}"
-
+    if cls._Timer__hours < 10:
+        if cls._Timer__minutes < 10:
+            if cls._Timer__seconds < 10:
+                return f"0{cls._Timer__hours}:0{cls._Timer__minutes}:0{cls._Timer__seconds}"
+            else:
+                return f"0{cls._Timer__hours}:0{cls._Timer__minutes}:{cls._Timer__seconds}"
+        else:
+            if cls._Timer__seconds < 10:
+                return f"0{cls._Timer__hours}:{cls._Timer__minutes}:0{cls._Timer__seconds}"
+            else:
+                return f"0{cls._Timer__hours}:{cls._Timer__minutes}:{cls._Timer__seconds}"
+    else:
+        if cls._Timer__minutes < 10:
+            if cls._Timer__seconds < 10:
+                return f"{cls._Timer__hours}:0{cls._Timer__minutes}:0{cls._Timer__seconds}"
+            else:
+                return f"{cls._Timer__hours}:0{cls._Timer__minutes}:{cls._Timer__seconds}"
+        else:
+            if cls._Timer__seconds < 10:
+                return f"{cls._Timer__hours}:{cls._Timer__minutes}:0{cls._Timer__seconds}"
+            else:
+                return f"{cls._Timer__hours}:{cls._Timer__minutes}:{cls._Timer__seconds}"
+    
 timer = Timer(23, 59, 59)
+print(timer)
+timer.next_second()
+print(timer)
+timer.prev_second()
 print(timer)
